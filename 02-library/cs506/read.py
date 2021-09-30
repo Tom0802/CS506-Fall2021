952e955061a5a86facba8e96258c464f35de5576
Tom0802 <90859888+Tom0802@users.noreply.github.com>
2021-09-30 13:20:25 -0400

    Update read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,21 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    X = []
+    f = open(csv_file_path, "r")
+    while(True):
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip().replace('"', '').split(',')
+        x = []
+        for i in line:
+            if i.isdecimal():
+                x.append(int(i))
+            elif i.isdigit():
+                x.append(float(i))
+            else:
+                x.append(str(i))
+        X.append(x)
+    f.close
+    return X
